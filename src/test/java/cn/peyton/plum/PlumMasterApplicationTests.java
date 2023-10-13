53815a19693b210b727edf3c8e13d0946101d6a2
peyton2919 <fz2919@tom.com>
2023-10-13 23:52:04 +0800

    update 20231013 22:19

---
+++ src/test/java/cn/peyton/plum/PlumMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.peyton.plum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
